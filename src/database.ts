5846db7dc60d7373136210aceaa18ffe9d6a7ff8
Igor Ramadas <igor@devv.com>
2019-09-04 00:08:03 +0200

    Adding support for database encryption.

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,11 @@
 
 import BaseEvents = require("./base-events")
 
+const env = process.env
 const lowdb = require("lowdb")
+const crypto = require("crypto")
+const logger = require("anyhow")
+const settings = require("setmeup").settings
 
 /**
  * Manages data stored by the service, data stored on the /database.json file.
@@ -36,7 +40,66 @@ class Database extends BaseEvents {
      */
     async init() {
         const FileAsync = require("lowdb/adapters/FileAsync")
-        this.db = await lowdb(new FileAsync("database.json"))
+
+        // Database should be encrypted?
+        if (settings.database.crypto.enabled) {
+            const cryptoCipher = env["SETMEUP_CRYPTO_CIPHER"] || settings.database.crypto.cipher
+            const cryptoKey = env["SETMEUP_CRYPTO_KEY"] || settings.database.crypto.key
+            const IV_LENGTH = 16
+            const NONCE_LENGTH = 5
+
+            // Helper to encrypt the database.
+            const encrypt = data => {
+                const nonce = crypto.randomBytes(NONCE_LENGTH)
+                const iv = Buffer.alloc(IV_LENGTH)
+                nonce.copy(iv)
+
+                const cipher = crypto.createCipheriv(cryptoCipher, cryptoKey, iv)
+                const encrypted = cipher.update(data.toString())
+                return Buffer.concat([nonce, encrypted, cipher.final()]).toString("base64")
+            }
+
+            // Helper to decrypt the database. Returns the data itself if failed to decrypt.
+            const decrypt = data => {
+                try {
+                    const message = Buffer.from(data, "base64")
+                    const iv = Buffer.alloc(IV_LENGTH)
+                    message.copy(iv, 0, 0, NONCE_LENGTH)
+
+                    const encryptedText = message.slice(NONCE_LENGTH)
+                    const decipher = crypto.createDecipheriv(cryptoCipher, cryptoKey, iv)
+                    let decrypted = decipher.update(encryptedText)
+                    return Buffer.concat([decrypted, decipher.final()]).toString()
+                } catch (ex) {
+                    logger.error("Database.decrypt", ex)
+                    return data
+                }
+            }
+
+            // Encryptor options passed to the FileAsync adapter.
+            const encryptor = {
+                serialize: data => {
+                    try {
+                        return encrypt(JSON.stringify(data, null, 0))
+                    } catch (ex) {
+                        logger.error("Database.serialize", ex)
+                        return data
+                    }
+                },
+                deserialize: data => {
+                    try {
+                        return JSON.parse(decrypt(data))
+                    } catch (ex) {
+                        logger.error("Database.deserialize", ex)
+                        return JSON.parse(data)
+                    }
+                }
+            }
+
+            this.db = await lowdb(new FileAsync("database.json.enc", encryptor))
+        } else {
+            this.db = await lowdb(new FileAsync("database.json"))
+        }
 
         // Write defaults.
         this.db.defaults({jsClient: {}, payments: [], emails: []}).write()
